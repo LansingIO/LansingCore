780e7c5e75027b1a3dc4ff762a837eefc472a110
Lansing Nye-Madden <me@lansing.io>
2014-12-05 08:13:00 -0500

    Maintained Coding Standards
    Or something like that....

--- src/io/lansing/core/command/CommandExecutedEvent.java
+++ src/io/lansing/core/command/CommandExecutedEvent.java
@@ -13,6 +13,7 @@ import org.bukkit.event.HandlerList;
  */
 
 public final class CommandExecutedEvent extends Event {
+	
     private static final HandlerList handlers = new HandlerList();
     
     private CommandOrigin origin;
